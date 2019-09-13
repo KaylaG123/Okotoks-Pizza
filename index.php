317159d27c188837434b8428c87be23a7549cb9f
Kayla The Great <owner@MacBook-Pro.local>
2019-09-12 19:58:58 -0600

    color change

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,maximum-scale=1">
+        <meta name="description" content="We make Pizza! We are Okotoks's leading Pizza place. We've got the Worlds Best Pizza - Our Specials today - Friday's Half Priced Pizza">
+        <meta name="author"      content="Kayla">
+        <meta name="keywords"    content="pizza, okotoks, south, calgary">
+        <title>Okotoks Pizza | The Best Pizza South of Calgary</title>
+        <link rel="shortcut icon" type="image.png" href="images/okotoks-pizza-favicon.png"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <!-- https://getbootstrap.com/docs/4.3/components/navbar/ -->
+        <!-- https://medium.com/wdstack/bootstrap-4-custom-navbar-1f6a2da5ed3c -->
+        <div class="wrapper">
+            <header id="top">
+                <nav class="navbar navbar-dark bg-custom">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="home.html">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="menu.html">Menu</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.html">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <!-- https://getbootstrap.com/docs/4.3/components/buttons/ -->
+                <a class="btn btn-danger mobile-phone" href="tel:403-555-555" role="button">CALL</a>
+                <h2 class="desktop-phone">403-555-555</h2>
+                <img src="images/logo.png" class="logo" alt="logo">
+                <p class="hours-location">
+                    Closed Forever
+                    <span class="open"> 10am-2am</span>
+            </header>
+            <main>
+                <!-- https://getbootstrap.com/docs/4.3/components/card/ -->
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <div class="card ratio">
+                            <div>
+                                <h5 class="card-header">Come Join Us!</h5>
+                                <div class="card-body">
+                                    <div class="map">
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2526.1157306633286!2d-113.9824759842589!3d50.717792079513174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5371982fae972385%3A0x8c59bcdcb7df95aa!2sPanago+Pizza!5e0!3m2!1sen!2sca!4v1557455113004!5m2!1sen!2sca" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- https://getbootstrap.com/docs/4.3/components/carousel/ -->
+                    <div class="col-sm-12 col-md-6">
+                        <h3>Check Out Our Specials Below!</h3>
+                        <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active" data-interval="10000">
+                                    <img src="images/special1.png" class="d-block w-100" alt="special1">
+                                </div>
+                                <div class="carousel-item" data-interval="2000">
+                                    <img src="images/special2.png" class="d-block w-100" alt="special2">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/special3.png" class="d-block w-100" alt="special3">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="video">
+                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/psVx2q9TMps" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <div class="row">
+                <div class="col">
+                    <footer>
+                        <ul>
+                            <li>
+                                <a href="#top">Top of Page</a>
+                            <li>
+                                <a href="https://www.google.ca/search?q=pizza%20google%20reviews&rlz=1C5CHFA_enCA723CA745&oq=pizza+google+reviews&aqs=chrome..69i57j0l5.9127j0j7&sourceid=chrome&ie=UTF-8&npsic=0&rflfq=1&rlha=0&rllag=51025527,-114060658,1267&tbm=lcl&rldimm=14003720437699374181&lqi=ChRwaXp6YSBnb29nbGUgcmV2aWV3cyIFOAGIAQFaBwoFcGl6emE&phdesc=nl7d5BCvr6I&ved=2ahUKEwiMq87TwMnhAhVHs54KHfivBsAQvS4wAXoECAoQIQ&rldoc=1&tbs=lrf:!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:1#lrd=0x5371706f0360e175:0xc257358b0ef2ec65,1,,,&rlfi=hd:;si:14003720437699374181,l,ChRwaXp6YSBnb29nbGUgcmV2aWV3cyIFOAGIAQFaBwoFcGl6emE,y,nl7d5BCvr6I;mv:!1m2!1d51.046712199999995!2d-114.0113241!2m2!1d50.9873404!2d-114.0996582;tbs:lrf:!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:1">Google Reviews</a>
+                            <li>
+                                <a href="careers">Careers</a>
+                            <li>
+                                <a href="mailto:info@okotoks.com">Email Us</a>
+                            <li>
+                                <p class="footer-hours">Open 7 Days a Week 10am-2pm</p>
+                            <li>
+                                <p>Connect with us
+                                   <img src="images/facebook.png" class="icon" alt="social">
+                                   <img src="images/insta.jpg" class="icon" alt="insta">
+                                   <img src="images/twitter.jpeg" class="icon" alt="twitter">
+                               </p>
+                            <li>
+                                <p>Okotoks Pizza Restaurant Copyright &copy; 2019-Okotoks Pizza. All rights reserved.
+                                </p>
+                        </ul>
+                    </footer>
+                </div>
+            </div>
+        </div>
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
